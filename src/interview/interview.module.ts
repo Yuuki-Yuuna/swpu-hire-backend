a0725568d4b705c0b2f2e60c285121aefb9b54d6
yuuki yuuna <2281363234@qq.com>
2025-03-06 09:13:31 +0800

    feat: 优化面试管理

--- src/interview/interview.module.ts
+++ src/interview/interview.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
+import { User, UserSchema } from '@/user/schema/user.schema'
 import { Job, JobSchema } from '@/job/schema/job.schema'
 import { Interview, InterviewSchema } from './schema/interview.schema'
 import { InterviewController } from './interview.controller'
@@ -8,6 +9,7 @@ import { InterviewService } from './interview.service'
 @Module({
   imports: [
     MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
       { name: Interview.name, schema: InterviewSchema },
       { name: Job.name, schema: JobSchema }
     ])
--- src/interview/interview.service.ts
+++ src/interview/interview.service.ts
@@ -2,14 +2,16 @@ import { HttpStatus, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { Job } from '@/job/schema/job.schema'
+import { User } from '@/user/schema/user.schema'
 import { Company } from '@/company/schema/company.schema'
 import { Interview } from './schema/interview.schema'
 import { createResponse } from '@/common/response'
-import { ApplyStatus } from '@/common/enum'
+import { ApplyStatus, UserType } from '@/common/enum'
 
 @Injectable()
 export class InterviewService {
   constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
     @InjectModel(Job.name) private jobModel: Model<Job>,
     @InjectModel(Interview.name) private interviewModel: Model<Interview>
   ) {}
@@ -20,6 +22,11 @@ export class InterviewService {
   }
 
   async createRecord(userId: string, jobId: string) {
+    const user = await this.userModel.findById(userId).exec()
+    if (user?.userType !== UserType.Student) {
+      return createResponse(null, { code: HttpStatus.BAD_REQUEST, message: '非法用户' })
+    }
+
     const jobResult = await this.jobModel.findById(jobId).populate('company').exec()
     if (!jobResult) {
       return createResponse(null, { code: HttpStatus.BAD_REQUEST, message: '职位不存在' })
@@ -36,6 +43,9 @@ export class InterviewService {
       applyTime: Date.now(),
       status: ApplyStatus.Apply
     })
+
+    const candidateTotal = (await this.interviewModel.find({ jobId }).exec()).length
+    await this.jobModel.findByIdAndUpdate(jobId, { $set: { candidateTotal } })
 
     return createResponse(null)
   }
